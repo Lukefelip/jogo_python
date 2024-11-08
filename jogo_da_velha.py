b90226341c2f369816be16035163c1a333b66087
jfmiranda2 <jfavmir@gmail.com>
2024-11-07 22:23:18 -0300

    aula 15

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -80,6 +80,8 @@ def faz_jogada():
     return status
 
 def check_vencedor():
+
+    
     status = False
     if q1 == q2 == q3 != '':
         pygame.draw.line(screen, 'orange' ,(50, 100), (550, 100), 10)
@@ -91,14 +93,19 @@ def check_vencedor():
         pygame.draw.line(screen, 'orange' ,(50, 500), (550, 500), 10)
         status = True
     elif q1 == q4 == q7 != '':
+        pygame.draw.line(screen, 'orange', (100, 50), (100, 550), 10)
         status = True
     elif q2 == q5 == q8 != '':
+        pygame.draw.line(screen, 'orange', (300, 50), (300, 550), 10)
         status = True
     elif q3 == q6 == q9 != '':
+        pygame.draw.line(screen, 'orange', (500, 50),  (500, 550), 10)
         status = True
     elif q1 == q5 == q9 != '':
+        pygame.draw.line(screen, 'orange', (50, 50), (550, 550), 10)
         status = True
     elif q3 == q5 == q7 != '':
+        pygame.draw.line(screen, 'orange', (550, 50), (50, 550), 10)
         status = True
     return status
 
